a53accfd3bc0b9573fdfd1ed627dd8f64422a50a
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-01-23 07:31:54 +0530

    Create makefish.sh

---
+++ makefish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+unzip -v &> /dev/null ||  apt install -y unzip
+make -v &> /dev/null ||  apt install -y make
+g++ -v &> /dev/null ||  apt install -y build-essential
+
+wget https://github.com/ddugovic/Stockfish/archive/master.zip
+unzip master.zip
+cd Stockfish-master/src
+
+if [ "$(g++ -Q -march=native --help=target | grep mpopcnt | grep enabled)" ] ; then
+  make profile-build ARCH=x86-64-modern COMP=gcc
+  strip stockfish
+  mv stockfish ../../stockfish_x64_modern
+  make clean
+fi
+
+cd
